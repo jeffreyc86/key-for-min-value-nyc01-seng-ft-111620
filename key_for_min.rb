533c95fc6b4b372f59eba2c56e6cbd4e8d607f92
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-02 15:28:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |item, index|
-    min_value = 
+  name_hash.each do |key, value|
+    min_value = name_hash[key][value]
     if name_hash[item][index] < min_value
       min_value = name_hash[item][index]
     end
