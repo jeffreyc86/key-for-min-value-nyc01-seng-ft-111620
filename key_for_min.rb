baa0841b99ef726406623bf2a38bcdb270d02fb8
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-02 15:30:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    min_value = name_hash[key][value]
+    min_value = INFINITY
     if name_hash[key][value] < min_value
       min_value = name_hash[key][value]
     end
